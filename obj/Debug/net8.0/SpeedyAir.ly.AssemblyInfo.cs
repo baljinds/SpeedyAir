17673bab962390eb8e2a8129ae3c2cc48359019d
baljinds <baljinder.sandhu21@gmail.com>
2024-10-16 15:51:34 -0400

    Add .gitignore and remove build files from tracking

--- obj/Debug/net8.0/SpeedyAir.ly.AssemblyInfo.cs
+++ obj/Debug/net8.0/SpeedyAir.ly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpeedyAir.ly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98e593900e833fc0b5974815745071387769f5d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpeedyAir.ly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpeedyAir.ly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
